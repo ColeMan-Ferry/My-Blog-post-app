4719c8964563590f558844dd841c24d5fb4211ba
KISAMBIRA256 <ibrahkisambira139@gmail.com>
2025-01-15 14:00:52 -0800

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+    <article>
+        <h1><a href="/post">My 1st Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore pariatur nemo ex corrupti unde, nulla accusamus impedit dolorem quis, repellendus, ipsum harum quia recusandae magni. Provident numquam nam voluptatum accusamus veritatis recusandae, eaque nemo cum alias dolorum, consequuntur consequatur excepturi vitae. Ullam architecto dolor tempora sed quae a odit. Excepturi!</p>
+    </article>
+    <br>
+    <hr>
+    <br>
+    <article>
+        <h1><a href="/posts/my-second-post">My 2nd Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore pariatur nemo ex corrupti unde, nulla accusamus impedit dolorem quis, repellendus, ipsum harum quia recusandae magni. Provident numquam nam voluptatum accusamus veritatis recusandae, eaque nemo cum alias dolorum, consequuntur consequatur excepturi vitae. Ullam architecto dolor tempora sed quae a odit. Excepturi!</p>
+    </article>
+    <br>
+    <hr>
+    <br>
+    <article>
+        <h1><a href="/posts/my-third-post">My 3rd Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore pariatur nemo ex corrupti unde, nulla accusamus impedit dolorem quis, repellendus, ipsum harum quia recusandae magni. Provident numquam nam voluptatum accusamus veritatis recusandae, eaque nemo cum alias dolorum, consequuntur consequatur excepturi vitae. Ullam architecto dolor tempora sed quae a odit. Excepturi!</p>
+    </article>
+    
+</body>
+</html>
